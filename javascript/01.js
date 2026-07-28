926228f0137c702dee7895b7d2dece269097fe85
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-07-28 20:48:30 +0530

    Javascript

---
+++ javascript/01.js
@@ -0,0 +1,22 @@
+console.log("Hello World");
+//data type primitive non-primitive
+//#Primitive
+//string
+let name="Prince Gupta";
+console.log(name);
+console.log(typeof(name))
+
+//number
+let a=45;
+let b=45;
+console.log(a+b);
+console.log(typeof(a+b))
+
+//symbol (data type)
+//bigint
+//int
+//null
+//undefined
+
+let A=undefined;
+console.log(typeof(A));
