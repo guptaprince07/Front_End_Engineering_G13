c3be2cc958eb78cf7f2daca2fc69dfe4197578a0
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-07-28 20:57:07 +0530

    js

--- javascript/01.js
+++ javascript/01.js
@@ -14,9 +14,25 @@ console.log(typeof(a+b))
 
 //symbol (data type)
 //bigint
+let r=0.11111111111111;
+console.log(r)
+console.log(typeof(r));
 //int
-//null
-//undefined
+let y=0.123456;
+console.log(y);
+console.log(typeof(y));
 
-let A=undefined;
-console.log(typeof(A));
+//null
+// let x=null;
+// console.log(x);
+// console.log(typeof(x));
+// //null ka value hamesa object milega
+
+// //undefined
+// let A=undefined;
+// console.log(typeof(A));
+
+//Boolean
+let flag=true;
+console.log(flag)
+console.log(typeof(flag));
